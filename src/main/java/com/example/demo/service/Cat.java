f34c31acbdedb7c934a98ff886801031cea7a11e
11kid <2207286148@qq.com>
2021-05-06 13:19:01 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/service/Cat.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+public abstract class Cat {
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/Garfield.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+
+import com.example.demo.service.Cat;
+
+public class Garfield extends Cat {
+
+}
